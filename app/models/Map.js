b012ef383fe56f7f71ff0021d430da6cb9f57059
Adam Hotz <ahotz@groupon.com>
2014-07-26 17:07:58 -0500

    some more animation stuff

--- app/models/Map.js
+++ app/models/Map.js
@@ -17,7 +17,7 @@ function initialize_map(zoom) {
     return divvy_map;
 }
 
-function createMarker(marker_position, marker_title, capacity) {
+function createMarker(marker_position, marker_title, capacity, animation) {
     var iconBackgroundColor = "2FCAFC"; //Chicago Blue
     var iconTextColor = "FFFFFF"; // White
     var marker = new google.maps.Marker({       
@@ -26,7 +26,7 @@ function createMarker(marker_position, marker_title, capacity) {
         // The google chart api is capable of generating map pin images
         // containing a number, letter or icon
         icon: "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=" + capacity + "|" + iconBackgroundColor + "|" + iconTextColor,
-        animation: google.maps.Animation.DROP,
+        animation: animation
     }); 
     // When a user clicks on a marker, a "click" event is generated
 	// Here is an example of animating a marker when it is clicked on
@@ -50,14 +50,21 @@ function paint_stations_on_map(stations, map, delay_per_station) {
 		var station = stations[station_idx];
 		var position = new google.maps.LatLng(station.latitude, station.longitude);
 		var title = station.stationName + "(" + station.stationId + ")";
-   		markers.push(createMarker(position, title, station.capacity, map));
+   		var animation = null;
+   		if (delay_per_station > 0) {
+   			animation = google.maps.Animation.DROP;
+   			markers.push(createMarker(position, title, station.capacity, animation));
+   		} else {
+   			var marker = createMarker(position, title, station.capacity, animation); 
+   			marker.setMap(map);
+   		}
 	}
 	for (var i=0; i < markers.length; ++i) {
 		setTimeout(function(i) {
 			return function() {
 				markers[i].setMap(map);
 			}
-		}(i), i * 50);
+		}(i), i * delay_per_station);
 	}
 } 
 
